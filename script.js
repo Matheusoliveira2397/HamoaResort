8cf93db59ef62d32a94eddbb545645daad8878e2
Matheus <matheus.oliveira@mozaiko.io>
2025-10-01 22:47:37 -0300

    feat: finaliza ajustes do site

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 1; i <= totalImages; i++) {
         const img = document.createElement('img');
         // Padroniza o nome do arquivo para sempre ter dois dígitos
-        const fileName = i === 1 ? 'hamoa1.jpeg' : 
-                   i === 2 ? 'hamoa2.jpeg' :
-                   i === 3 ? 'hamoa3.jpeg' :
-                   i === 4 ? 'hamoa4.jpeg' :
-                   i === 5 ? 'hamoa5.jpeg' :
-                   i === 6 ? 'hamoa6.jpeg' :
-                   i === 7 ? 'hamoa7.jpeg' :
-                   i === 8 ? 'hamoa8.jpeg' :
-                   i === 9 ? 'hamoa9.jpeg' :
+        const fileName = i === 1 ? 'Hamoa1.jpeg' : 
+                   i === 2 ? 'Hamoa2.jpeg' :
+                   i === 3 ? 'Hamoa3.jpeg' :
+                   i === 4 ? 'Hamoa4.jpeg' :
+                   i === 5 ? 'Hamoa5.jpeg' :
+                   i === 6 ? 'Hamoa6.jpeg' :
+                   i === 7 ? 'Hamoa7.jpeg' :
+                   i === 8 ? 'Hamoa8.jpeg' :
+                   i === 9 ? 'Hamoa9.jpeg' :
                    `Hamoa${i}.jpeg`;
         img.src = fileName;
         img.alt = `Imagem ${i} do Hamoa Resort`;
